697768a66591dfbb625a61ea6839ac0443b61543
Adam <adambartonjones@gmail.com>
2023-12-07 18:44:09 +0000

    updated

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,10 +1,25 @@
+local telescope = require("telescope")
+local actions = require("telescope.actions")
+
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
     "nvim-telescope/telescope-fzf-native.nvim",
     build = "make",
     config = function()
-      require("telescope").load_extension("fzf")
+      telescope.load_extension("fzf")
     end,
+  },
+  opts = {
+    defaults = {
+      mappings = {
+        i = {
+          ["<C-s>"] = actions.select_vertical,
+          ["<C-d>"] = actions.delete_buffer,
+          ["<C-e>"] = actions.preview_scrolling_up,
+          ["<C-y>"] = actions.preview_scrolling_down,
+        },
+      },
+    },
   },
 }
