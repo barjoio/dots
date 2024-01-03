f2d58a9d372a50b642fd635d439300a4d1a1f396
adam <adambartonjones@gmail.com>
2024-01-03 16:30:23 +0000

    updated

---
+++ script/wini.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp -r ~/.config/LazyVim ~/AppData/Local
+cp -r ~/.config/alacritty ~/AppData/Roaming
+cp -r ~/.config/bat ~/AppData/Roaming
+# cat ~/.config/.gitconfig >> ~/.gitconfig
+cp -r ~/.config/nvim ~/AppData/Local
